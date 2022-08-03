7c76c46c097ae9452d70aba8f408c59f7648ee70
Jayasoorya <jayasoorya.jithendra@synoriq.in>
2022-08-03 18:14:07 +0530

    custom hook useResults

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,30 +3,11 @@ import React, { useState, useEffect } from "react";
 
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setError] = useState("");
-
-  const searchApi = async (searchTerm) => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setError("Something Went wrong");
-    }
-  };
-
-  useEffect(()=>{
-      searchApi("pasta");
-  },[])
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
